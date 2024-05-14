67fa2c7bb77ace5c04ff7da297a856e75296a7a5
imran61999 <smemrannit@gmail.com>
2024-05-14 08:11:53 +0600

    api for increase quantity is ready

--- index.js
+++ index.js
@@ -30,17 +30,27 @@ async function run() {
     // const userCollection = client.db('juteDB').collection('user');
     const booksCollection = client.db('library').collection('books');
     const categoryCollection = client.db('library').collection('category');
+    const borrowCollection = client.db('library').collection('borrowed');
 
     // category related api
     app.get('/category',async(req, res)=>{
       const category = await categoryCollection.find().toArray();
       res.send(category)
     })
+
+    app.get('/details/:id', async(req, res) =>{
+      const id = req.params;
+      const query = {_id: new ObjectId(id)};
+      const book = await booksCollection.findOne(query);
+      res.send(book);
+    })
     // same category data
     app.get('/sameCategory/:category', async(req, res)=>{
       const mainCategory= req.params.category;
-      const query ={category: mainCategory}
+      console.log(mainCategory)
+      const query ={category_name: mainCategory}
       const result = await booksCollection.find(query).toArray();
+      console.log(result);
       res.send(result)
     })
 
@@ -57,6 +67,20 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/borrow/books', async(req, res)=>{
+      const email = req.query.email;
+      const query ={user: email};
+      const result = await borrowCollection.find(query).toArray();
+      res.send(result);
+    })
+    app.post('/borrow/books', async(req, res)=>{
+      const borrowInfo = req.body;
+      const result = await borrowCollection.insertOne(borrowInfo);
+      res.send(result)
+    })
+
+
+
     // books read for update
     app.get('/updateBook/:id', async(req, res) =>{
       const id = req.params.id;
@@ -65,6 +89,23 @@ async function run() {
       const result = await booksCollection.findOne(query);
       res.send(result)
     })
+
+    app.patch('/book/:id/decrease', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await booksCollection.updateOne(query, { $inc: {quantity:-1}})
+      console.log(result);
+      res.send(result)
+    })
+    // increase
+    app.patch('/book/:id/increase', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await booksCollection.updateOne(query, { $inc: {quantity:1}})
+      console.log(result);
+      res.send(result)
+    })
+
     app.put('/update/:id', async(req, res) =>{
       const id = req.params.id;
       console.log('id for update ',id)
